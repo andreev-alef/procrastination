db6106f37764a682cc2e4508c422d383d4d55ee2
andreev_af <andreev_af@10.0.0.10>
2016-12-14 11:24:56 +0700

    группа 2

---
+++ js/gr2.js
@@ -0,0 +1,76 @@
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+function getNum() {
+    var ran = 0;
+    while (ran < 10) {
+        ran = Math.floor(Math.random() * 100);
+    }
+    return ran;
+}
+
+$(document).ready(function () {
+
+    var exams = [
+        {"a": getNum(), "b": getNum()},
+        {"a": getNum(), "b": getNum()},
+        {"a": getNum(), "b": getNum()},
+        {"a": getNum(), "b": getNum()},
+        {"a": getNum(), "b": getNum()}
+    ]
+
+    var N = exams.length;
+
+    var i = 0;
+    
+    var pre = document.getElementsByClassName('.preview-example');
+    var Npre = pre.length;
+    alert(Npre);
+    
+    $('#step').html(i + 1);
+    $('#A').html(exams[i].a);
+    $('#B').html(exams[i].b);
+    $('#btn_pre').animate({'opacity': 0}, 'fast');
+    $('#btn_pre').css({'cursor': 'default'});
+    
+    
+
+    $('#btn_next').click(function () {
+        i++;
+        if (i < N) {
+            $('#step').html(i + 1);
+            $('#A').html(exams[i].a);
+            $('#B').html(exams[i].b);
+            $('#btn_pre').animate({'opacity': 1}, 'fast');
+            $('#btn_pre').css({'cursor': 'pointer'});
+        }
+        if (i === (N - 1)) {
+            $('#btn_next').animate({'opacity': 0}, 'fast');
+            $('#btn_next').css({'cursor': 'default'});
+        }
+        if (i > N) {
+            i = N - 1;
+        }
+    });
+    $('#btn_pre').click(function () {
+        i--;
+        if (i >= 0) {
+            $('#step').html(i + 1);
+            $('#A').html(exams[i].a);
+            $('#B').html(exams[i].b);
+            $('#btn_next').animate({'opacity': 1}, 'fast');
+        }
+        if (i === 0) {
+            $('#btn_pre').animate({'opacity': 0}, 'fast');
+            $('#btn_pre').css({'cursor': 'default'});
+
+        }
+        if (i < 0) {
+            i = 0;
+        }
+    });
+});
+
--- js/main.js
+++ js/main.js
@@ -17,66 +17,66 @@ var font_family = ['Allegretto',
     'PT Sans',
     'PT Serif'];
 
-function getNum() {
-    var ran = 0;
-    while (ran < 10) {
-        ran = Math.floor(Math.random() * 100);
-    }
-    return ran;
-}
-
-$(document).ready(function () {
-
-    var exams = [
-        {"a": getNum(), "b": getNum()},
-        {"a": getNum(), "b": getNum()},
-        {"a": getNum(), "b": getNum()},
-        {"a": getNum(), "b": getNum()},
-        {"a": getNum(), "b": getNum()}
-    ]
-
-    var N = exams.length;
-
-    var i = 0;
-    $('#step').html(i + 1);
-    $('#A').html(exams[i].a);
-    $('#B').html(exams[i].b);
-    $('#btn_pre').animate({'opacity': 0}, 'fast');
-    $('#btn_pre').css({'cursor': 'default'});
-
-    $('#btn_next').click(function () {
-        i++;
-        if (i < N) {
-            $('#step').html(i + 1);
-            $('#A').html(exams[i].a);
-            $('#B').html(exams[i].b);
-            $('#btn_pre').animate({'opacity': 1}, 'fast');
-            $('#btn_pre').css({'cursor': 'pointer'});
-        }
-        if (i === (N - 1)) {
-            $('#btn_next').animate({'opacity': 0}, 'fast');
-            $('#btn_next').css({'cursor': 'default'});
-        }
-        if (i > N) {
-            i = N - 1;
-        }
-    });
-    $('#btn_pre').click(function () {
-        i--;
-        if (i >= 0) {
-            $('#step').html(i + 1);
-            $('#A').html(exams[i].a);
-            $('#B').html(exams[i].b);
-            $('#btn_next').animate({'opacity': 1}, 'fast');
-        }
-        if (i === 0) {
-            $('#btn_pre').animate({'opacity': 0}, 'fast');
-            $('#btn_pre').css({'cursor': 'default'});
-
-        }
-        if (i < 0) {
-            i = 0;
-        }
-    });
-});
-
+//function getNum() {
+//    var ran = 0;
+//    while (ran < 10) {
+//        ran = Math.floor(Math.random() * 100);
+//    }
+//    return ran;
+//}
+//
+//$(document).ready(function () {
+//
+//    var exams = [
+//        {"a": getNum(), "b": getNum()},
+//        {"a": getNum(), "b": getNum()},
+//        {"a": getNum(), "b": getNum()},
+//        {"a": getNum(), "b": getNum()},
+//        {"a": getNum(), "b": getNum()}
+//    ]
+//
+//    var N = exams.length;
+//
+//    var i = 0;
+//    $('#step').html(i + 1);
+//    $('#A').html(exams[i].a);
+//    $('#B').html(exams[i].b);
+//    $('#btn_pre').animate({'opacity': 0}, 'fast');
+//    $('#btn_pre').css({'cursor': 'default'});
+//
+//    $('#btn_next').click(function () {
+//        i++;
+//        if (i < N) {
+//            $('#step').html(i + 1);
+//            $('#A').html(exams[i].a);
+//            $('#B').html(exams[i].b);
+//            $('#btn_pre').animate({'opacity': 1}, 'fast');
+//            $('#btn_pre').css({'cursor': 'pointer'});
+//        }
+//        if (i === (N - 1)) {
+//            $('#btn_next').animate({'opacity': 0}, 'fast');
+//            $('#btn_next').css({'cursor': 'default'});
+//        }
+//        if (i > N) {
+//            i = N - 1;
+//        }
+//    });
+//    $('#btn_pre').click(function () {
+//        i--;
+//        if (i >= 0) {
+//            $('#step').html(i + 1);
+//            $('#A').html(exams[i].a);
+//            $('#B').html(exams[i].b);
+//            $('#btn_next').animate({'opacity': 1}, 'fast');
+//        }
+//        if (i === 0) {
+//            $('#btn_pre').animate({'opacity': 0}, 'fast');
+//            $('#btn_pre').css({'cursor': 'default'});
+//
+//        }
+//        if (i < 0) {
+//            i = 0;
+//        }
+//    });
+//});
+//
